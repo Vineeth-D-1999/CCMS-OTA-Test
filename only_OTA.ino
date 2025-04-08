bc0e9cadad96a68fb73297ff325fd62bf73b4bd2
Harish-H-C <73733752+Harish-H-C@users.noreply.github.com>
2025-04-08 12:02:33 +0530

    Version 1.3

--- only_OTA.ino
+++ only_OTA.ino
@@ -15,7 +15,7 @@ const char* version_url = "https://raw.githubusercontent.com/Vineeth-D-1999/CCMS
 const char* firmware_url = "https://raw.githubusercontent.com/Vineeth-D-1999/CCMS-OTA-Test/master/only_OTA.ino.bin";
 
 // Current firmware version
-const String currentVersion = "1.2";
+const String currentVersion = "1.3";
 
 void setup() {
   Serial.begin(115200);
@@ -23,7 +23,6 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
-    Serial.println("Connecting...");
   };
   if (checkForUpdate()) {
     performOTA();
